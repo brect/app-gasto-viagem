56e65e8b73d93918ab80864b3d51cfbb10910e0f
brect <b.limas93@gmail.com>
2020-08-26 20:48:44 -0300

    logica validacao implementada;

--- app/src/main/java/com/blimas/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/blimas/gastodeviagem/MainActivity.kt
@@ -2,10 +2,43 @@ package com.blimas.gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.buttonCalculate) {
+            calcular()
+        }
+    }
+
+    private fun calcular() {
+        if (validationForm()) {
+            val distance = editDistance.text.toString().toFloat()
+            val price = editPrice.text.toString().toFloat()
+            val autonomy = editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price / autonomy)
+            textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else {
+
+            Toast.makeText(this, getString(R.string.alert_fields_form), Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    private fun validationForm(): Boolean {
+        return (editDistance.text.toString() != ""
+                && editPrice.text.toString() != ""
+                && editAutonomy.text.toString() != "")
     }
 }
